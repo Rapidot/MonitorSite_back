978147ce150762c7edf1903a846018a7d1ee067f
Rapido <hpakkane@nic.fi>
2020-01-14 14:46:30 +0200

    Initiate app.2

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,5 @@
 
- if (process.env.NODE_ENV !== 'production') {
+ /*if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
@@ -9,7 +9,17 @@ let MONGODB_URI = process.env.MONGODB_URI
 if (process.env.NODE_ENV === 'test') {
   PORT = process.env.TEST_PORT
   MONGODB_URI = process.env.TEST_MONGODB_URI
+}*/
+
+require('dotenv').config()
+
+let PORT = process.env.PORT
+let MONGODB_URI = process.env.MONGODB_URI
+
+if (process.env.NODE_ENV === 'test') {
+  mongoUrl = process.env.TEST_MONGODB_URI
 }
+
 
 /* NPM START testi
 require('dotenv').config()
